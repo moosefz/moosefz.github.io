75927917eeae7f170c701bf58614700450db3ca1
Moose Fawaz <moosefawaz@gmail.com>
2020-07-02 14:46:27 -0400

    Add new page. Add navbar with JS script.

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// Scroll changes navbar colour from transparent to grey
+$(function () {
+    $(document).scroll(function () {
+        var $nav = $("#mainNavbar");
+        $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
+    });
+});
