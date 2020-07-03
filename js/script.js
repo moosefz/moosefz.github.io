69cad68064383f9e1330389450e222cce8991e68
Moose Fawaz <moosefawaz@gmail.com>
2020-07-03 10:21:59 -0400

    Add open of repo tab on each project click

--- js/script.js
+++ js/script.js
@@ -5,3 +5,13 @@ $(function () {
         $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
     });
 });
+
+
+// Multiple tab open
+document.getElementById("weather-wa").addEventListener("click", (evt) => {
+    window.open("https://github.com/moosefz/weather-wa");
+})
+
+document.getElementById("io-msg").addEventListener("click", (evt) => {
+    window.open("https://github.com/moosefz/socket-messenger");
+})
